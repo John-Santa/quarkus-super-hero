dabacd671ad6209fc0bf13064ecb7a216d604598
John Santa <andersonsanta3@gmail.com>
2022-09-13 14:56:55 -0500

    Add: Creacion de paths y configuracion de persistencia

--- super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
+++ super-heroes/rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
@@ -1,16 +1,65 @@
 package io.quarkus.workshop.superheroes.fight;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import java.util.List;
+
+import static javax.ws.rs.core.MediaType.*;
 
 @Path("/api/fights")
+@Produces(APPLICATION_JSON)
 public class FightResource {
+
+    @Inject Logger logger;
+
+    @Inject
+    FightService service;
+
+    @GET
+    @Path("/randomfighters")
+    public Response getRandomFighters() {
+        Fighters fighters = service.findRandomFighters();
+        logger.debug("Get random fighters " + fighters);
+        return Response.ok(fighters).build();
+    }
+
+    @GET
+    public Response getAllFights() {
+        List<Fight> fights = service.findAllFights();
+        logger.debug("Total number of fights " + fights);
+        return Response.ok(fights).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getFight(Long id) {
+        Fight fight = service.findFightById(id);
+        if (fight != null) {
+            logger.debug("Found fight " + fight);
+            return Response.ok(fight).build();
+        } else {
+            logger.debug("No fight found with id " + id);
+            return Response.noContent().build();
+        }
+    }
+
+    @POST
+    public Fight fight(@Valid Fighters fighters, UriInfo uriInfo) {
+        return service.persistFight(fighters);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello Fight Resource";
     }
 }
